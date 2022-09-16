dbcbf1b23d10bab055054cc28600da0cd9e4ab50
tblandin <tiarra.blandin@outlook.com>
2022-09-15 22:04:39 -0600

    initial commit

---
+++ MenrvaBooksJPA/src/main/java/com/menrva/menrvabooksjpa/MenrvaBooksJpaApplication.java
@@ -0,0 +1,13 @@
+package com.menrva.menrvabooksjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenrvaBooksJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenrvaBooksJpaApplication.class, args);
+    }
+
+}
---
+++ MenrvaBooksJPA/src/test/java/com/menrva/menrvabooksjpa/MenrvaBooksJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menrva.menrvabooksjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenrvaBooksJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
