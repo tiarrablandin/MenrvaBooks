78b72620faf768b3660f27e1e20a6e8bff25d741
Tristan Slaunwhite <williamslaunwhite@gmail.com>
2024-03-10 18:09:57 -0700

    added new security config

---
+++ menrvaBackend/spring/src/main/kotlin/com/menrva/spring/security/SecurityConfig.kt
@@ -0,0 +1,43 @@
+package com.menrva.spring.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    private lateinit var userDetailsService: UserDetailsService
+    private lateinit var passwordEncoder: PasswordEncoder
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors { it.disable() }
+            .csrf() { it.disable() }
+            .authorizeHttpRequests { requests ->
+                requests
+                    .requestMatchers("/", "/home", "/api/test", "/authenticate").permitAll()
+                    .anyRequest().authenticated()
+            }
+        return http.build()
+    }
+
+    @Bean
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
+        return authenticationConfiguration.authenticationManager
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+}
