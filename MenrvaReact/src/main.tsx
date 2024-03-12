1612fabb5e22352c0c09d93cd3830a45ab64a212
tblandin <tiarra.blandin@outlook.com>
2024-03-12 15:58:57 -0600

    Nav & Foot

--- MenrvaReact/src/main.tsx
+++ MenrvaReact/src/main.tsx
@@ -2,12 +2,16 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import CustomNavbar from './components/Navbar/CustomNavbar';
+import { Footer } from './components/Footer/Footer';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <CustomNavbar />
     <App />
+    <Footer />
   </StrictMode>,
 );
