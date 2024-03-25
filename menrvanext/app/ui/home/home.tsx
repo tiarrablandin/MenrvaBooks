bfdb6c11124164f779eb2ba40497f4d328468e51
Tristan Slaunwhite <williamslaunwhite@gmail.com>
2024-03-25 16:33:09 -0700

    merging into main

--- menrvanext/app/ui/home/home.tsx
+++ menrvanext/app/ui/home/home.tsx
@@ -7,7 +7,7 @@ import BookSkeleton from '../book/bookSkeleton';
 const HomeComponent: React.FC = () => {
   return (
     <div className='w-screen h-full flex flex-col items-center justify-start'>
-      <BookSlider fetchData={fetchBooks} title='All Books'/>
+      <BookSlider fetchData={fetchBooks} title='All Books' />
     </div>
   )
 }
