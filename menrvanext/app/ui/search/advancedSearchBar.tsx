5a1af95301f12c671927ab95ee4aacb7a6279820
Tristan Slaunwhite <williamslaunwhite@gmail.com>
2024-04-05 02:23:02 -0700

    mostly fixed input border

--- menrvanext/app/ui/search/advancedSearchBar.tsx
+++ menrvanext/app/ui/search/advancedSearchBar.tsx
@@ -41,15 +41,16 @@ const AdvancedSearchBar = () => {
     return (
         <form onSubmit={handleSubmit} className='flex flex-row w-full gap-3 m-2 justify-between'>
             <Input
-                className={`${lusi.className} w-4/5 !border-2`}
+                className={`${lusi.className} w-4/5 border-eggplant placeholder-shown:border-2 placeholder-shown:border-eggplant placeholder-shown:border-t-eggplant placeholder-shown:border-t-2`}
                 type="text"
                 size="lg"
                 placeholder="Search for books, authors, genres..."
                 label="Search"
                 // * for no label:
-                // labelProps={{
-                //     className: "before:content-none after:content-none content-none",
-                // }}
+                labelProps={{
+                    // className: "before:content-none after:content-none content-none",
+                    className: "before:border-l-2 before:border-t-2 focus:before:!content-none after:!mt-[6px] before:!mt-[6px] ",
+                }}
                 containerProps={{
                     className: "min-w-0 "
                 }}
--- menrvanext/app/ui/search/suggestionCards.tsx
+++ menrvanext/app/ui/search/suggestionCards.tsx
@@ -10,8 +10,8 @@ const SuggestionCards = () => {
 
   return (
     <List className="-mt-1 rounded w-[72.5%] mr-auto ml-1 flex flex-col p-0 py-1">
-      {suggestions.map((book) => (
-        <ListItem className="p-1 hover:bg-eggplant/60 -my-1">
+      {suggestions.map((book, key) => (
+        <ListItem key={key} className="p-1 hover:bg-eggplant/60 -my-1">
           <SuggestionCard key={book.id} book={book} />
         </ListItem>
       ))}
