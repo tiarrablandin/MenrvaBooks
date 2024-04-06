4baac21832267a6f19c478f1050990cefc0d1360
tblandin <tiarra.blandin@outlook.com>
2024-04-06 12:36:54 -0600

    added admin files

---
+++ menrvanext/app/admin/(overview)/page.tsx
@@ -0,0 +1,14 @@
+import AdminPortal from "@/app/ui/adminPortal/adminPortal";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MenrvaBooks",
+};
+
+export default async function Page() {
+  return (
+    <main className="w-screen min-h-[calc(100vh-295px)]">
+      <AdminPortal/>
+    </main>
+  );
+}
---
+++ menrvanext/app/ui/adminPortal/searchCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const searchCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default searchCard
