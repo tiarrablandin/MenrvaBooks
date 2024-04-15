f0eb177cd90a464098cecbf8dda78599b7bfcd74
tblandin <tiarra.blandin@outlook.com>
2024-04-15 13:30:04 -0600

    sidebar built & nav optional

--- menrvanext/app/ui/adminPortal/adminPortal.tsx
+++ menrvanext/app/ui/adminPortal/adminPortal.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SearchTable from './searchTable'
 import AdminTabs from './adminTabs'
+import Sidebar from './sidebar'
 
 const adminPortal = () => {
   return (
     <div className="w-full h-full">
+      <Sidebar/>
       <AdminTabs />
       {/* <SearchTable/> */}
     </div>
