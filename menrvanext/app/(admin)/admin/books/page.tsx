c4cec0f99da40563f58e4f7b303e0e79bb036181
Tristan Slaunwhite <williamslaunwhite@gmail.com>
2024-04-30 18:40:29 -0700

    added tabs for admin tables

--- menrvanext/app/(admin)/admin/books/page.tsx
+++ menrvanext/app/(admin)/admin/books/page.tsx
@@ -1,10 +1,31 @@
 import DynamicTable from "@/app/ui/adminPortal/table/dynamicTable";
+import { Tab, TabPanel, Tabs, TabsBody, TabsHeader } from "@/providers";
 
 export default async function Page() {
     return (
         <main className="min-h-[calc(100vh-295px)] w-[calc(100%-4rem)] mr-1 ml-auto">
-            <DynamicTable entityType="books" />
-            <DynamicTable entityType="series" />
+            <Tabs value="books" className="py-8">
+                <TabsHeader className="flex justify-center bg-eggplant w-80 mx-auto">
+                    <Tab value="books" className="">
+                        Books
+                    </Tab>
+                    <Tab value="series" className="">
+                        Series
+                    </Tab>
+                </TabsHeader>
+                <TabsBody>
+                    <TabPanel value="books">
+                        <div className="flex justify-center gap-8">
+                            <DynamicTable entityType="books" />
+                        </div>
+                    </TabPanel>
+                    <TabPanel value="series">
+                        <div className="flex justify-center pt-8 gap-8">
+                            <DynamicTable entityType="series" />
+                        </div>
+                    </TabPanel>
+                </TabsBody>
+            </Tabs>
         </main>
     );
 }
--- menrvanext/app/(admin)/admin/users/page.tsx
+++ menrvanext/app/(admin)/admin/users/page.tsx
@@ -1,10 +1,31 @@
 import DynamicTable from "@/app/ui/adminPortal/table/dynamicTable";
+import { Tab, TabPanel, Tabs, TabsBody, TabsHeader } from "@/providers";
 
 export default async function Page() {
     return (
         <main className="min-h-[calc(100vh-295px)] w-[calc(100%-4rem)] mr-1 ml-auto">
-            <DynamicTable entityType="users" />
-            <DynamicTable entityType="comments" />
+            <Tabs value="users" className="py-8">
+                <TabsHeader className="flex justify-center bg-eggplant w-80 mx-auto">
+                    <Tab value="users" className="">
+                        Users
+                    </Tab>
+                    <Tab value="comments" className="">
+                        Comments
+                    </Tab>
+                </TabsHeader>
+                <TabsBody>
+                    <TabPanel value="users">
+                        <div className="flex justify-center gap-8">
+                            <DynamicTable entityType="users" />
+                        </div>
+                    </TabPanel>
+                    <TabPanel value="comments">
+                        <div className="flex justify-center pt-8 gap-8">
+                            <DynamicTable entityType="comments" />
+                        </div>
+                    </TabPanel>
+                </TabsBody>
+            </Tabs>
         </main>
     );
 }
