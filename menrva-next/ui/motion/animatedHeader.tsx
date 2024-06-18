7ec666f06105995d346e691306de4072ac6ebc30
Tristan Slaunwhite <williamslaunwhite@gmail.com>
2024-06-18 15:55:19 -0700

    landing page formatting

--- menrva-next/ui/motion/animatedHeader.tsx
+++ menrva-next/ui/motion/animatedHeader.tsx
@@ -8,7 +8,7 @@ export default function AnimatedHeader() {
         animate={{ opacity: 1 }}
         transition={{ duration: 2 }}
         // className="text-3xl font-bold mb-4 text-black"
-        className="text-3xl font-bold mb-4 bg-gradient-to-r from-indigo-300 to-red-800 inline-block text-transparent bg-clip-text "
+        className="text-3xl font-bold mb-4 bg-gradient-to-r from-indigo-300 to-red-800 inline-block text-transparent bg-clip-text"
     >
         Find Your Favorite Books, Authors, and Series
     </motion.h1>
