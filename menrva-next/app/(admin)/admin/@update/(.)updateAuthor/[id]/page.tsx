2246d43314de634ffb39d245f1731b7b714654fc
Tristan Slaunwhite <williamslaunwhite@gmail.com>
2024-06-28 14:36:04 -0700

    add/update book and author modals

---
+++ menrva-next/app/(admin)/admin/@update/(.)updateAuthor/[id]/page.tsx
@@ -0,0 +1,12 @@
+import UpdateBook from "@/ui/adminPortal/book/updateBook";
+import React from "react";
+
+const UpdateBookModal = () => {
+    return (
+        <>
+            <UpdateBook />
+        </>
+    )
+}
+
+export default UpdateBookModal;
--- menrva-next/app/(admin)/admin/addBook/page.tsx
+++
@@ -1,7 +0,0 @@
-import AddBook from "@/ui/adminPortal/book/addBook";
-
-export default function Page(){
-  <>
-  <AddBook/>
-  </>
-}
