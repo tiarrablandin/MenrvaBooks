3fdccd6ad037c9c56e3f20c3584b9ada31291fe1
tblandin <tiarra.blandin@outlook.com>
2024-07-11 15:50:26 -0600

    register stepper done

--- menrva-next/ui/register/dataSelections.tsx
+++ menrva-next/ui/register/dataSelections.tsx
@@ -3,7 +3,6 @@
 import { Typography } from "@/providers/coreProviders";
 import React, { useEffect, useState } from "react";
 import DataChip from "./dataChip";
-import { color } from "framer-motion";
 
 interface DataConfigType {
   apiEndpoint: string;
@@ -46,75 +45,64 @@ const fetchData = async (apiEndpoint: string): Promise<any[]> => {
   }
 };
 
-const DataSelections: React.FC = () => {
-  const [data, setData] = useState<{ [key: string]: any[] }>({});
-  const [selectedChips, setSelectedChips] = useState<{ [key: string]: Set<string> }>({});
+interface DataSelectionsProps {
+  type: string;
+}
+
+const DataSelections: React.FC<DataSelectionsProps> = ({ type }) => {
+  const [data, setData] = useState<any[]>([]);
+  const [selectedChips, setSelectedChips] = useState<Set<string>>(new Set());
 
   const chipColors = ["bg-deep-sea", "bg-rose", "bg-eggplant", "bg-medium-malachite"];
   const getColorClass = (index: number) => chipColors[index % chipColors.length];
 
   useEffect(() => {
-    const fetchDataForAllTypes = async () => {
-      let allData: { [key: string]: any[] } = {};
-      for (const type of Object.keys(dataConfig)) {
-        const endpoint = dataConfig[type].apiEndpoint;
-        const fetchedData = await fetchData(endpoint);
-        allData[type] = fetchedData.map((item: any, index: number) => ({
+    const fetchDataForType = async () => {
+      const endpoint = dataConfig[type].apiEndpoint;
+      const fetchedData = await fetchData(endpoint);
+      setData(
+        fetchedData.map((item: any, index: number) => ({
           ...item,
           color: getColorClass(Math.floor(Math.random() * index)),
-        }));
-      }
-      setData(allData);
-
-      setSelectedChips(
-        Object.keys(dataConfig).reduce((acc, key) => {
-          acc[key] = new Set();
-          return acc;
-        }, {} as { [key: string]: Set<string> })
+        }))
       );
     };
 
-    fetchDataForAllTypes();
-  }, []);
+    fetchDataForType();
+  }, [type]);
 
-  const handleChipClick = (type: string, item: any) => {
+  const handleChipClick = (item: any) => {
     setSelectedChips((prevSelected) => {
-      const updated = new Set(prevSelected[type]);
+      const updated = new Set(prevSelected);
       if (updated.has(item.name)) {
         updated.delete(item.name);
       } else {
         updated.add(item.name);
       }
-      return { ...prevSelected, [type]: updated };
+      return updated;
     });
   };
 
-  {
-    return (
-      <div>
-        {Object.keys(data).map((type) => (
-          <div key={type} className="mx-20">
-            <Typography variant="h2" className="mt-4">
-              {dataConfig[type].pageTitle}
-            </Typography>
-            <Typography className="mb-2">{dataConfig[type].description}</Typography>
-            <div className="flex flex-wrap">
-              {data[type] &&
-                data[type].map((item, index) => (
-                  <DataChip
-                    key={index}
-                    item={item}
-                    color={item.color}
-                    selected={selectedChips[type].has(item.name)}
-                    onClick={() => handleChipClick(type, item)}
-                  />
-                ))}
-            </div>
-          </div>
-        ))}
+  return (
+    <div className="mx-20">
+      <Typography variant="h2" className="mt-4">
+        {dataConfig[type].pageTitle}
+      </Typography>
+      <Typography className="mb-2">{dataConfig[type].description}</Typography>
+      <div className="flex flex-wrap">
+        {data &&
+          data.map((item, index) => (
+            <DataChip
+              key={index}
+              item={item}
+              color={item.color}
+              selected={selectedChips.has(item.name)}
+              onClick={() => handleChipClick(item)}
+            />
+          ))}
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default DataSelections;
--- menrva-next/ui/register/register.tsx
+++ menrva-next/ui/register/register.tsx
@@ -1,32 +1,34 @@
-"use client"
+"use client";
 
-import { Button, Step, Stepper } from '@/providers/coreProviders';
-import React from 'react';
-import DataSelections from './dataSelections';
-import ProfileInfo from './profileInfo';
+import { Button, Step, Stepper } from "@/providers/coreProviders";
+import React from "react";
+import DataSelections from "./dataSelections";
+import ProfileInfo from "./profileInfo";
+
+const stepTypes = ["profile", "genres", "subgenres", "keywords", "tags"];
 
 const Register = () => {
   const [activeStep, setActiveStep] = React.useState(0);
   const [isLastStep, setIsLastStep] = React.useState(false);
   const [isFirstStep, setIsFirstStep] = React.useState(false);
- 
+
   const handleNext = () => !isLastStep && setActiveStep((cur) => cur + 1);
   const handlePrev = () => !isFirstStep && setActiveStep((cur) => cur - 1);
   return (
-      <div className="w-full py-4 px-8">
+    <div className="w-full py-4 px-8">
       <Stepper
         activeStep={activeStep}
         isLastStep={(value) => setIsLastStep(value)}
         isFirstStep={(value) => setIsFirstStep(value)}
       >
-        <Step className="h-4 w-4" onClick={() => setActiveStep(0)} />
-        <Step className="h-4 w-4" onClick={() => setActiveStep(1)} />
-        <Step className="h-4 w-4" onClick={() => setActiveStep(2)} />
+        {stepTypes.map((_, index) => (
+          <Step key={index} className="h-4 w-4" onClick={() => setActiveStep(index)} />
+        ))}
       </Stepper>
       <div className="mt-8">
-        {activeStep === 0 && <ProfileInfo/>}
-        {activeStep === 1 && <DataSelections/>}
-        </div>
+        {activeStep === 0 && <ProfileInfo />}
+        {activeStep > 0 && <DataSelections type={stepTypes[activeStep]} />}
+      </div>
       <div className="mt-16 flex justify-between">
         <Button onClick={handlePrev} disabled={isFirstStep}>
           Prev
@@ -36,7 +38,7 @@ const Register = () => {
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
